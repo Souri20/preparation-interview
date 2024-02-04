8f3392e266f97291c0e2271ad69bf44fb040f7bd
Sourabh Kumbhar <sourabh.kumbhar@slkgroup.com>
2024-02-04 18:35:11 +0530

    04/05

--- 04_straing1.js
+++ 04_straing1.js
@@ -1,11 +1,34 @@
-//  ######### find how many vowels in string #######
+// //  ######### find how many vowels in string #######
 
-let str = "i love you"
-let vowel =['i','o','e','u','a']
-count =0;
-for(let word of str){
-    if (vowel.includes(word)){
-        count +=1
-    }
-}
-console.log(count)
+// let str = "i love you"
+// let vowel =['i','o','e','u','a']
+// count =0;
+// for(let word of str){
+//     if (vowel.includes(word)){
+//         count +=1
+//     }
+// // }
+// // console.log(count)
+
+// // ######## number is prime or not ##########
+
+// let num = 7;
+// const checkPrime =(num)=>{
+//     isPrime = true;
+//     for (let i=2; i<num/2; i++){
+//         if (num%i == 0){
+//             isPrime =false
+//             break
+            
+//         }
+//     }
+//     if(isPrime){
+//         return `${num} is prime`
+//     }else{
+//         return `${num} is not prime`
+//     }
+// }
+
+// console.log(checkPrime(num))
+
+console.log(57%2)
--- 05_basic.js
+++ 05_basic.js
@@ -1,1 +1,14 @@
-// need to write code
+// sum of all elements
+
+let arr=[2,4,6,43,2]
+
+let Sum =(arr)=>{
+    let sum =0;
+    arr.forEach((val)=>{
+        sum +=val
+    })
+    console.log(sum)
+    return sum
+    
+}
+Sum(arr)
